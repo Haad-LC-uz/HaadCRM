ad4d53d6eb938a7aa2ca6bc8c67142ad81ec4a9b
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-14 13:27:45 -0700

    Created Group Entity

---
+++ src/HaadCRM.Domain/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using HaadCRM.Domain.Commons;
+
+namespace HaadCRM.Domain.Entities;
+
+public class Group : Auditable
+{
+    public Course Course { get; set; }
+    public Employee TeacherId { get; set; }
+    public Employee AssistantId { get; set; }
+    public string Name { get; set; }
+    public DateTime StartTime { get; set; }
+    public int Duration { get; set; }
+    public DateTime EndTime { get; set; }
+    public decimal Price { get; set; }
+    public string Room { get; set; }
+}
