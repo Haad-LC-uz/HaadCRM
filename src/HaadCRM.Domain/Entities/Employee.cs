8974bdf536b0330acf67a4d23618ca5a7515c964
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-14 13:05:31 -0700

    created employee entity

---
+++ src/HaadCRM.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using HaadCRM.Domain.Commons;
+using HaadCRM.Domain.Enums;
+
+namespace HaadCRM.Domain.Entities;
+
+public class Employee : Auditable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Description { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+    public decimal Salary { get; set; }
+    public string PicturePath { get; set; }
+    public Role Role { get; set; }
+}
