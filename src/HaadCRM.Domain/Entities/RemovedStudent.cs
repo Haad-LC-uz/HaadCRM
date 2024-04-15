cd18a28a6eaac7ad156103904b84c462c08ca20f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-15 13:21:04 +0500

    Wrote RemovedStudent

--- src/HaadCRM.Domain/Entities/RemovedStudent.cs
+++ src/HaadCRM.Domain/Entities/RemovedStudent.cs
@@ -1,6 +1,12 @@
-﻿namespace HaadCRM.Domain.Entities
+﻿using HaadCRM.Domain.Commons;
+
+namespace HaadCRM.Domain.Entities;
+
+public class RemovedStudent : Auditable
 {
-    internal class RemovedStudent
-    {
-    }
+    public long StudentId { get; set; }
+    public Student Student { get; set; }
+    public long GroupId { get; set; }
+    public Group Group { get; set; }
+    public string Reason { get; set; }
 }
