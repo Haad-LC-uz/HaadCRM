f8a2e83781a538ef5a3e7df6eb32968ab202000d
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-15 08:13:21 -0700

    Created Lesson dtos

---
+++ src/HaadCRM.Service/DTOs/Lessons/LessonUpdateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HaadCRM.Service.DTOs.Lessons;
+
+public class LessonUpdateModel
+{
+    public long GroupId { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/HaadCRM.Service/DTOs/Lessons/LessonViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HaadCRM.Service.DTOs.Groups;
+
+namespace HaadCRM.Service.DTOs.Lessons;
+
+public class LessonViewModel
+{
+    public long Id { get; set; }
+    public GroupViewModel Group { get; set; }
+    public string Name { get; set; }
+}
