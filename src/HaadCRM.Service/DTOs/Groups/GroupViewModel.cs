8228ce8c87d7444a80ce47401d7fefed5d9c74be
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-15 07:44:36 -0700

    Created Group Dtos

---
+++ src/HaadCRM.Service/DTOs/Groups/GroupViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HaadCRM.Service.DTOs.Employees;
+
+namespace HaadCRM.Service.DTOs.Groups;
+
+public class GroupViewModel
+{
+    public long Id { get; set; }
+    public long Course { get; set; }
+    public EmployeeViewModel Teacher { get; set; }
+    public EmployeeViewModel Assistant { get; set; }
+    public string Name { get; set; }
+    public DateTime StartTime { get; set; }
+    public int Duration { get; set; }
+    public DateTime EndTime { get; set; }
+    public decimal Price { get; set; }
+    public string Room { get; set; }
+}
