840f20f3ef4d0ac87b23d050e38a0dca2eaf42dd
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-16 18:38:39 +0500

    fixed AttendanceViewModel

--- src/HaadCRM.Service/DTOs/Attendencies/AttendanceViewModel.cs
+++ src/HaadCRM.Service/DTOs/Attendencies/AttendanceViewModel.cs
@@ -5,6 +5,7 @@ namespace HaadCRM.Service.DTOs.Attendencies;
 
 public class AttendanceViewModel
 {
+    public long Id { get; set; }
     public Student Student { get; set; }
     public Lesson Lesson { get; set; }
     public bool IsAttended { get; set; }
