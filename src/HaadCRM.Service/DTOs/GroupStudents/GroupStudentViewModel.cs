39b7789a9e9abaf108c3c86f7b5e7a91c923076c
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-16 18:27:57 +0500

    Wrote GroupStudentViewModel

--- src/HaadCRM.Service/DTOs/GroupStudents/GroupStudentViewModel.cs
+++ src/HaadCRM.Service/DTOs/GroupStudents/GroupStudentViewModel.cs
@@ -1,5 +1,12 @@
-﻿namespace HaadCRM.Service.DTOs.GroupStudents;
+﻿using HaadCRM.Domain.Entities.Students;
+using System.Text.RegularExpressions;
+
+namespace HaadCRM.Service.DTOs.GroupStudents;
 
 public class GroupStudentViewModel
 {
+    public Group Group { get; set; }
+    public Student Student { get; set; }
+    public bool IsPaid { get; set; }
+    public bool IsPassed { get; set; }
 }
