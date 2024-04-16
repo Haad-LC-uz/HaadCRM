6a86774886503ab862f0421e97f90d2b90e42894
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-16 04:47:48 -0700

    created Exam dtos

---
+++ src/HaadCRM.Service/DTOs/Exams/ExamCreateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HaadCRM.Service.DTOs.Exams;
+
+public class ExamCreateModel
+{
+    public long TeacherId { get; set; }
+    public long AssistantId { get; set; }
+    public long GroupId { get; set; }
+    public string PicturePath { get; set; }
+    public DateTime DateOfExam { get; set; }
+    public DateTime DeadLine { get; set; }
+}
