c539893263874b3db58cc8ca0090b88cdea24914
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-16 04:27:19 -0700

    Employee dtos changed according to the etities

--- src/HaadCRM.Service/DTOs/Employees/EmployeeViewModel.cs
+++ src/HaadCRM.Service/DTOs/Employees/EmployeeViewModel.cs
@@ -1,16 +1,11 @@
-﻿using HaadCRM.Domain.Enums;
-
-namespace HaadCRM.Service.DTOs.Employees;
+﻿namespace HaadCRM.Service.DTOs.Employees;
 
 public class EmployeeViewModel
 {
     public long Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public long UserId { get; set; }
+    public long EmployeeRoleId { get; set; }
+    public long AssetId { get; set; }
     public string Description { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
     public decimal Salary { get; set; }
-    public string PicturePath { get; set; }
-    public Role Role { get; set; }
 }
