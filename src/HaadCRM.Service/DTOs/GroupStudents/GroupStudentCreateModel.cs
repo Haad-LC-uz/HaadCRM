409f1293ce895043e35fbf1aa70e9df8a249e7a6
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-16 18:07:42 +0500

    Wrote GroupSudentCreateModel

--- src/HaadCRM.Service/DTOs/GroupStudents/GroupStudentCreateModel.cs
+++ src/HaadCRM.Service/DTOs/GroupStudents/GroupStudentCreateModel.cs
@@ -1,6 +1,9 @@
-﻿namespace HaadCRM.Service.DTOs.GroupStudents
+﻿namespace HaadCRM.Service.DTOs.GroupStudents;
+
+public class GroupStudentCreateModel
 {
-    internal class GroupStudentCreateModel
-    {
-    }
+    public long GroupId { get; set; }
+    public long StudentId { get; set; }
+    public bool IsPaid { get; set; }
+    public bool IsPassed { get; set; }
 }
