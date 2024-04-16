f00db21d8d152dd1559187b28a4cdf5cc21c1593
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-16 17:01:46 +0500

    created and wrote AttencanceViewModel

---
+++ src/HaadCRM.Service/DTOs/Attendencies/AttendanceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HaadCRM.Domain.Entities.Lessons;
+using HaadCRM.Domain.Entities.Students;
+
+namespace HaadCRM.Service.DTOs.Attendencies;
+
+public class AttendanceViewModel
+{
+    public Student Student { get; set; }
+    public Lesson Lesson { get; set; }
+    public bool IsAttended { get; set; }
+}
