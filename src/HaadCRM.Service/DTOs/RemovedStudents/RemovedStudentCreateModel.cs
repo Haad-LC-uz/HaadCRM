2328de9706827190c126df3736b1523cd6868556
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-16 18:30:41 +0500

    Wrote RemovedStudentCreateModel

--- src/HaadCRM.Service/DTOs/RemovedStudents/RemovedStudentCreateModel.cs
+++ src/HaadCRM.Service/DTOs/RemovedStudents/RemovedStudentCreateModel.cs
@@ -1,6 +1,8 @@
-﻿namespace HaadCRM.Service.DTOs.RemovedStudents
+﻿namespace HaadCRM.Service.DTOs.RemovedStudents;
+
+public class RemovedStudentCreateModel
 {
-    internal class RemovedStudentCreateModel
-    {
-    }
+    public long StudentId { get; set; }
+    public long GroupId { get; set; }
+    public string Reason { get; set; }
 }
