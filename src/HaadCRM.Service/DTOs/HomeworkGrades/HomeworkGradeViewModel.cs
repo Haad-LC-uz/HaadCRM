ae7d7fe7006ba7956999d1dc1fe70f86675cadb3
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-16 20:31:24 -0700

    changed Homework grade dtos

---
+++ src/HaadCRM.Service/DTOs/HomeworkGrades/HomeworkGradeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HaadCRM.Service.DTOs.Employees;
+using HaadCRM.Service.DTOs.Homeworks;
+using HaadCRM.Service.DTOs.Students;
+
+namespace HaadCRM.Service.DTOs.HomeworkGrades;
+
+public class HomeworkGradeViewModel
+{
+    public long Id { get; set; }
+    public StudentViewModel Student { get; set; }
+    public EmployeeViewModel Assistant { get; set; }
+    public HomeworkViewModel Homework { get; set; }
+    public int Grade { get; set; }
+}
