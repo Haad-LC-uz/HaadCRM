eea383a7c9e1ffd25e7edfa7cfa77f85dae2d772
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-17 03:19:01 -0700

    Created Repository

---
+++ src/HaadCRM.Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using HaadCRM.Domain.Commons;
+
+namespace HaadCRM.Data.Repositories;
+
+public interface IRepository<T> where T : Auditable
+{
+    Task<T> InsertAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task<T> DropAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null);
+    Task<IEnumerable<T>> SelectAsEnumerableAsync(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracked = true);
+    IQueryable<T> SelectAsQueryable(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracked = true);
+}
---
+++ src/HaadCRM.Data/Repositories/Repository.cs
@@ -0,0 +1,81 @@
+﻿using HaadCRM.Data.Contexts;
+using HaadCRM.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HaadCRM.Data.Repositories;
+
+public class Repository<T> : IRepository<T> where T : Auditable
+{
+    private readonly HaadDbContext context;
+    private readonly DbSet<T> set;
+    public Repository(HaadDbContext context)
+    {
+        this.context = context;
+        this.set = context.Set<T>();
+    }
+
+    public async Task<T> InsertAsync(T entity)
+    {
+        return (await set.AddAsync(entity)).Entity;
+    }
+    public async Task<T> UpdateAsync(T entity)
+    {
+        entity.UpdatedAt = DateTime.UtcNow;
+        set.Update(entity);
+        return await Task.FromResult(entity);
+    }
+
+    public async Task<T> DeleteAsync(T entity)
+    {
+        entity.IsDeleted = true;
+        entity.DeletedAt = DateTime.UtcNow;
+        set.Update(entity);
+        return await Task.FromResult(entity);
+    }
+
+    public async Task<T> DropAsync(T entity)
+    {
+        return await Task.FromResult(set.Remove(entity).Entity);
+    }
+
+    public async Task<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null)
+    {
+        var query = set.Where(expression);
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<T>> SelectAsEnumerableAsync(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracked = true)
+    {
+        var query = expression is null ? set : set.Where(expression);
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        if (!isTracked)
+            query.AsNoTracking();
+
+        return await query.ToListAsync();
+    }
+
+    public IQueryable<T> SelectAsQueryable(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracked = true)
+    {
+        var query = expression is null ? set : set.Where(expression);
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        if (!isTracked)
+            query.AsNoTracking();
+
+
+        return query;
+    }
+}
