64872c5f47f5e178b58be161f1648e9686755ba1
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-16 20:29:16 -0700

    Changed Exam dtos

--- src/HaadCRM.Service/DTOs/Exams/ExamCreateModel.cs
+++ src/HaadCRM.Service/DTOs/Exams/ExamCreateModel.cs
@@ -5,7 +5,7 @@ public class ExamCreateModel
     public long TeacherId { get; set; }
     public long AssistantId { get; set; }
     public long GroupId { get; set; }
-    public string PicturePath { get; set; }
+    public long AssetId { get; set; }
     public DateTime DateOfExam { get; set; }
     public DateTime DeadLine { get; set; }
 }
--- src/HaadCRM.Service/DTOs/Exams/ExamViewModel.cs
+++ src/HaadCRM.Service/DTOs/Exams/ExamViewModel.cs
@@ -1,12 +1,16 @@
-﻿namespace HaadCRM.Service.DTOs.Exams;
+﻿using HaadCRM.Service.DTOs.Assets;
+using HaadCRM.Service.DTOs.Employees;
+using HaadCRM.Service.DTOs.Groups;
+
+namespace HaadCRM.Service.DTOs.Exams;
 
 public class ExamViewModel
 {
     public long Id { get; set; }
-    public long TeacherId { get; set; }
-    public long AssistantId { get; set; }
-    public long GroupId { get; set; }
-    public string PicturePath { get; set; }
+    public EmployeeViewModel Teacher { get; set; }
+    public EmployeeViewModel Assistant { get; set; }
+    public GroupViewModel Group { get; set; }
+    public AssetViewModel ProfilePicture { get; set; }
     public DateTime DateOfExam { get; set; }
     public DateTime DeadLine { get; set; }
 }
