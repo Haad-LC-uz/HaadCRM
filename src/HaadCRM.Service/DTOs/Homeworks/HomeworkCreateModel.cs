f68d09702048c53ce030384e694165f681704f50
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-16 20:31:41 -0700

    changed Homework dtos

---
+++ src/HaadCRM.Service/DTOs/Homeworks/HomeworkCreateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HaadCRM.Service.DTOs.Homeworks;
+
+public class HomeworkCreateModel
+{
+    public long LessonId { get; set; }
+    public long AssistantId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DeadLine { get; set; }
+}
---
+++ src/HaadCRM.Service/DTOs/Homeworks/HomeworkViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HaadCRM.Service.DTOs.Employees;
+using HaadCRM.Service.DTOs.Lessons;
+
+namespace HaadCRM.Service.DTOs.Homeworks;
+
+public class HomeworkViewModel
+{
+    public long Id { get; set; }
+    public LessonViewModel Lesson { get; set; }
+    public EmployeeViewModel Assistant { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DeadLine { get; set; }
+}
