2bf762b502df45d3c3a2eaeee825d2086668ad0a
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 04:35:34 +0500

    Created CourseService

---
+++ src/HaadCRM.Service/Services/Courses/CourseService.cs
@@ -0,0 +1,31 @@
+﻿using HaadCRM.Service.DTOs.Courses;
+
+namespace HaadCRM.Service.Services.Courses;
+
+public class CourseService : ICourceService
+{
+    public Task<CourseViewModel> CreateAsync(CourseCreateModel course)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<CourseViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CourseViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CourseUpdateModel> UpdateAsync(long id, CourseUpdateModel course)
+    {
+        throw new NotImplementedException();
+    }
+}
