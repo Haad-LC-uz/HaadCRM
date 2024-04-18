5648f74b4e28ec3872604a549e5f6edd51f2eadc
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-19 00:29:46 +0500

    Interface Written

--- src/HaadCRM.Service/Services/UserRoles/IUserRoleService.cs
+++ src/HaadCRM.Service/Services/UserRoles/IUserRoleService.cs
@@ -1,6 +1,12 @@
-﻿namespace HaadCRM.Service.Services.UserRoles
+﻿using HaadCRM.Service.DTOs.UserDTOs.UserRoles;
+
+namespace HaadCRM.Service.Services.UserRoles;
+
+public interface IUserRoleService
 {
-    public interface IUserRoleService
-    {
-    }
+    ValueTask<UserRoleViewModel> CreateAsync(UserRoleCreateModel createModel);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<IEnumerable<UserRoleViewModel>> GetAllAsync();
+    ValueTask<UserRoleViewModel> GetByIdAsync(long id);
+    ValueTask<UserRoleViewModel> UpdateAsync(long id, UserRoleUpdateModel updateModel);
 }
