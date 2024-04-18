cada930a0b5974cf843116e0302dc819dda8dbca
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-18 15:02:57 +0500

    Pagination ready to use

---
+++ src/HaadCRM.Service/Configurations/FileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaadCRM.Service.Configurations;
+
+public enum FileType
+{
+    Images = 1,
+    Videos,
+    Audios
+}
