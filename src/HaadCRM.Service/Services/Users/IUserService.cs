68dc0310071a27e5afc1039caf82be762494670e
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-19 01:04:12 +0500

    UserService Ready

---
+++ src/HaadCRM.Service/Services/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using HaadCRM.Service.DTOs.UserDTOs.Users;
+
+namespace HaadCRM.Service.Services.Users
+{
+    public interface IUserService
+    {
+        ValueTask<UserViewModel> CreateAsync(UserCreateModel createModel);
+        ValueTask<bool> DeleteAsync(long id);
+        ValueTask<IEnumerable<UserViewModel>> GetAllAsync();
+        ValueTask<UserViewModel> GetByIdAsync(long id);
+        ValueTask<UserViewModel> UpdateAsync(long id, UserUpdateModel updateModel);
+    }
+}
