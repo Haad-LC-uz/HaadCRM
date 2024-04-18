c39ffb72d98c20938edb38af367d8bfc163d2241
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 04:34:48 +0500

    cleaned

--- src/HaadCRM.Service/Services/Courses/ICourceService.cs
+++ src/HaadCRM.Service/Services/Courses/ICourceService.cs
@@ -4,10 +4,9 @@ namespace HaadCRM.Service.Services.Courses;
 
 public interface ICourceService
 {
-
-   Task<CourseViewModel> CreateAsync(CourseCreateModel course);
+    Task<CourseViewModel> CreateAsync(CourseCreateModel course);
     Task<CourseUpdateModel> UpdateAsync(long id, CourseUpdateModel course);
     Task<bool> DeleteAsync(long id);
-   Task<CourseViewModel> GetByIdAsync(long id);
-   Task<IEnumerable<CourseViewModel>> GetAllAsync();
+    Task<CourseViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<CourseViewModel>> GetAllAsync();
 }
