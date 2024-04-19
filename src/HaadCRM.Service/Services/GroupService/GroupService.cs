73933bc86cc7e60025d933e7130bb1533be76b3f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 15:43:27 +0500

    created Groupservice

---
+++ src/HaadCRM.Service/Services/GroupService/GroupService.cs
@@ -0,0 +1,31 @@
+﻿using HaadCRM.Service.DTOs.GroupDTOs.Groups;
+
+namespace HaadCRM.Service.Services.GroupService;
+
+public class GroupService : IGroupService
+{
+    public ValueTask<GroupViewModel> CreateAsync(GroupCreateModel group)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(GroupCreateModel group)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<GroupViewModel> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<GroupViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<GroupViewModel> UpdateAsync(long id, GroupUpdateModel group)
+    {
+        throw new NotImplementedException();
+    }
+}
