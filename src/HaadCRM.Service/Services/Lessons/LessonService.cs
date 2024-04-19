4f18b2db72e9aa7bc6e203177382ed6ce2fa16f7
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 05:15:07 +0500

    Created LessonService

---
+++ src/HaadCRM.Service/Services/Lessons/LessonService.cs
@@ -0,0 +1,31 @@
+﻿using HaadCRM.Service.DTOs.LessonsDTOs.Lessons;
+
+namespace HaadCRM.Service.Services.Lessons;
+
+public class LessonService : ILessonService
+{
+    public Task<LessonViewModel> CreateAsync(LessonCreateModel lesson)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<LessonViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LessonViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LessonViewModel> UpdateAsync(long id, LessonUpdateModel lesson)
+    {
+        throw new NotImplementedException();
+    }
+}
