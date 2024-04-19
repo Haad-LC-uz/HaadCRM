814369fc75f1b15e44b60b93e473a28e0636b1b8
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-19 13:21:11 +0500

    Create method of service

---
+++ src/HaadCRM.Service/Services/Employees/EmployeeService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HaadCRM.Data.UnitOfWorks;
+using HaadCRM.Domain.Entities.Employees;
+using HaadCRM.Service.DTOs.EmployeeDTOs.Employees;
+using HaadCRM.Service.Exceptions;
+
+namespace HaadCRM.Service.Services.Employees;
+
+public class EmployeeService(IUnitOfWork unitOfWork, IMapper mapper)
+{
+    public async ValueTask<EmployeeViewModel> CreateAsync(EmployeeCreateModel createModel)
+    {
+        var existEmployee = await unitOfWork.Employees.SelectAsync(Employee => 
+            Employee.UserId == createModel.UserId);
+
+        if (existEmployee != null)
+        {
+            throw new AlreadyExistException("A user with the same email or phone already exists.");
+        }
+
+        var employee = mapper.Map<Employee>(createModel);
+
+        employee.EmployeeRole = await unitOfWork.EmployeeRoles.SelectAsync(id => id.Id == createModel.EmployeeRoleId);
+
+        var createdEmloyee = await unitOfWork.Employees.InsertAsync(employee);
+
+        await unitOfWork.SaveAsync();
+
+        return mapper.Map<EmployeeViewModel>(createdEmloyee);
+    }
+}
