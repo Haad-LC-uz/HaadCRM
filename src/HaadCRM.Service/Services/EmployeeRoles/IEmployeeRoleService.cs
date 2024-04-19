a7de0f7c950299c7405cebe379e67ea3d75a34a6
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-19 12:48:07 +0500

    Written Interface

---
+++ src/HaadCRM.Service/Services/EmployeeRoles/IEmployeeRoleService.cs
@@ -0,0 +1,13 @@
+﻿using HaadCRM.Service.DTOs.EmployeeDTOs.EmployeeRoles;
+
+namespace HaadCRM.Service.Services.EmployeeRoles
+{
+    public interface IEmployeeRoleService
+    {
+        ValueTask<EmployeeRoleViewModel> CreateAsync(EmployeeRoleCreateModel createModel);
+        ValueTask<bool> DeleteAsync(long id);
+        ValueTask<IEnumerable<EmployeeRoleViewModel>> GetAllAsync();
+        ValueTask<EmployeeRoleViewModel> GetByIdAsync(long id);
+        ValueTask<EmployeeRoleViewModel> UpdateAsync(long id, EmployeeRoleUpdateModel updateModel);
+    }
+}
