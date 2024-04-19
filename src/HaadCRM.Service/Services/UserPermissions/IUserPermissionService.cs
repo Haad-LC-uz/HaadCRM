4410c27fba3007ad7c1299e93cbca3d54c250335
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-19 19:56:10 +0500

    Interface updated

--- src/HaadCRM.Service/Services/UserPermissions/IUserPermissionService.cs
+++ src/HaadCRM.Service/Services/UserPermissions/IUserPermissionService.cs
@@ -5,7 +5,7 @@ namespace HaadCRM.Service.Services.UserPermissions;
 public interface IUserPermissionService
 {
     ValueTask<UserPermissionViewModel> CreateAsync(UserPermissionCreateModel createModel);
-    ValueTask<bool> DeleteAsync(long userId, long permissionId);
+    ValueTask<bool> DeleteAsync(long id);
     ValueTask<IEnumerable<UserPermissionViewModel>> GetAllAsync();
     ValueTask<UserPermissionViewModel> GetByIdAsync(long userId, long permissionId);
     ValueTask<UserPermissionViewModel> UpdateAsync(UserPermissionUpdateModel updateModel);
