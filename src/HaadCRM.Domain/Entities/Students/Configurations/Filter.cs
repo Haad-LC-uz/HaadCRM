f6b83b9991c0e75d4d0c823a4bd8d63bcce87996
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-04-19 13:19:56 +0500

    Created Student Service

---
+++ src/HaadCRM.Domain/Entities/Students/Configurations/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace HaadCRM.Service.Configurations;
+
+// Here are some relevant code fragments from other files of the repo:
+public class Filter
+{
+    public string OrderBy { get; set; }
+    public string OrderType { get; set; }
+}
---
+++ src/HaadCRM.Service/Services/Students/StudentService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using HaadCRM.Data.UnitOfWorks;
+using HaadCRM.Domain.Entities.Students;
+using HaadCRM.Service.DTOs.StudentDTOs.Students;
+using HaadCRM.Service.DTOs.UserDTOs.Users;
+using HaadCRM.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaadCRM.Service.Services.Students;
+
+public class StudentService(IUnitOfWork unitOfWork, IMapper mapper) : IStudentService
+{
+    public async ValueTask<StudentViewModel> CreateAsync(StudentCreateModel createModel)
+    {
+        var exist = unitOfWork.Students.SelectAsync(u => u.UserId == createModel.UserId);
+        if (exist != null)
+            throw new AlreadyExistException("This student is already exists");
+
+        await unitOfWork.Students.InsertAsync(mapper.Map<Student>(createModel));
+        await unitOfWork.SaveAsync();
+        return await Task.FromResult(mapper.Map<StudentViewModel>(createModel));
+    }
+
+    public async ValueTask<StudentViewModel> UpdateAsync(long id, StudentUpdateModel updateModel)
+    {
+        var student = await unitOfWork.Students.SelectAsync(user => user.Id == id)
+            ?? throw new NotFoundException($"Student with ID={id} is not found");
+        mapper.Map(updateModel, student);
+
+        await unitOfWork.Students.UpdateAsync(student);
+        await unitOfWork.SaveAsync();
+
+        return await Task.FromResult(mapper.Map<StudentViewModel>(student));
+    }
+
+    public async ValueTask<bool> DeleteAsync(long id)
+    {
+        var exist = await unitOfWork.Students.SelectAsync(s => s.Id == id)
+            ?? throw new NotFoundException($"This student with ID={id} is not found");
+
+        await unitOfWork.Students.DeleteAsync(exist);
+        await unitOfWork.SaveAsync();
+        return true;
+    }
+
+    public async ValueTask<StudentViewModel> GetByIdAsync(long id)
+    {
+        var student = await unitOfWork.Students.SelectAsync(s => s.Id == id)
+            ?? throw new NotFoundException($"This student with ID={id} is not found");
+
+        return await Task.FromResult(mapper.Map<StudentViewModel>(student));
+    }
+
+    public async ValueTask<IEnumerable<StudentViewModel>> GetAllAsync()
+    {
+        var students = await unitOfWork.Students.SelectAsEnumerableAsync();
+        return mapper.Map<IEnumerable<StudentViewModel>>(students);
+    }
+}
