3cec9f161ca81cf6c3cca13cc348351bd9d737d3
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 07:06:11 +0500

    cleaned

--- src/HaadCRM.Service/Services/LessonFiles/ILessonService.cs
+++ src/HaadCRM.Service/Services/LessonFiles/ILessonService.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace HaadCRM.Service.Services.LessonFiles;
 
-namespace HaadCRM.Service.Services.LessonFiles
+public class ILessonService
 {
-    internal class ILessonService
-    {
-    }
 }
