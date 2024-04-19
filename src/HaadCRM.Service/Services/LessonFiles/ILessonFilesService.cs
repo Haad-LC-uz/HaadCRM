c12e56cedac43e137c52c6faee4441de7507dc04
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 12:37:15 +0500

    Interface methods are changed

--- src/HaadCRM.Service/Services/LessonFiles/ILessonFilesService.cs
+++ src/HaadCRM.Service/Services/LessonFiles/ILessonFilesService.cs
@@ -4,9 +4,9 @@ namespace HaadCRM.Service.Services.LessonFiles;
 
 public interface ILessonFilesService
 {
-    Task<LessonFileViewModel> CreateAsync(LessonFileViewModel lessonfile);
-    Task<LessonFileViewModel> UpdateAsync(long id, LessonFileUpdateModel lessonfile);
-    Task<bool> DeleteAsync(long id);
-    Task<LessonFileViewModel> GetByIdAsync(long id);
-    Task<IEnumerable<LessonFileViewModel>> GetAllAsync();
+    ValueTask<LessonFileViewModel> CreateAsync(LessonFileViewModel lessonfile);
+    ValueTask<LessonFileViewModel> UpdateAsync(long id, LessonFileUpdateModel lessonfile);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<LessonFileViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<LessonFileViewModel>> GetAllAsync();
 }
