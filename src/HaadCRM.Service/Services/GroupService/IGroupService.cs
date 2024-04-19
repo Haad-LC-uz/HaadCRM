a1d85820d7512e3049f2f31bc0fb33bbd3536dd0
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 15:43:03 +0500

    Wrote Interface

--- src/HaadCRM.Service/Services/GroupService/IGroupService.cs
+++ src/HaadCRM.Service/Services/GroupService/IGroupService.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HaadCRM.Service.DTOs.GroupDTOs.Groups;
 
-namespace HaadCRM.Service.Services.GroupService
+namespace HaadCRM.Service.Services.GroupService;
+
+public interface IGroupService
 {
-    internal class IGroupService
-    {
-    }
+    ValueTask<GroupViewModel> CreateAsync(GroupCreateModel group);
+    ValueTask<GroupViewModel> UpdateAsync(long id, GroupUpdateModel group);
+    ValueTask<bool> DeleteAsync(GroupCreateModel group);
+    ValueTask<GroupViewModel> GetByIdAsync(long id);
+    ValueTask<GroupViewModel> GetAllAsync();
 }
