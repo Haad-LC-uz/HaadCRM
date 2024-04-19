bf9cf400eabc86299e284bd8ec07a6b9563a006a
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-19 05:14:10 +0500

    Wrote Interfaces

--- src/HaadCRM.Service/Services/Lessons/ILessonService.cs
+++ src/HaadCRM.Service/Services/Lessons/ILessonService.cs
@@ -1,5 +1,12 @@
-﻿namespace HaadCRM.Service.Services.Lessons;
+﻿using HaadCRM.Service.DTOs.LessonsDTOs.Lessons;
+
+namespace HaadCRM.Service.Services.Lessons;
 
 public interface LessonService
 {
+    Task<LessonViewModel> CreateAsync(LessonCreateModel lesson);
+    Task<LessonViewModel> UpdateAsync(long id, LessonUpdateModel lesson);
+    Task<bool> DeleteAsync(long id);
+    Task<LessonViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<LessonViewModel>> GetAllAsync();
 }
