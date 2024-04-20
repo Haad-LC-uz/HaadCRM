edd460dec18c2dbfc2f5a9392ec9b5cd3e76fed0
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-20 09:17:10 +0500

    Created GroupStudent service

---
+++ src/HaadCRM.Service/Services/GroupStudents/GroupStudent.cs
@@ -0,0 +1,31 @@
+﻿using HaadCRM.Service.DTOs.GroupDTOs.GroupStudents;
+
+namespace HaadCRM.Service.Services.GroupStudents;
+
+public class GroupStudent : IGroupStudent
+{
+    public ValueTask<GroupStudentViewModel> CreateAsync(GroupStudentCreateModel groupStudent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<IEnumerable<GroupStudentViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<GroupStudentViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<GroupStudentViewModel> UpdateAsync(long id, GroupStudentUpdateModel groupStudent)
+    {
+        throw new NotImplementedException();
+    }
+}
