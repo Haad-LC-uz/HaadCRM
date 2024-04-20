797044720f4f3755b4230eddf0bf347e129a4a82
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-20 09:16:39 +0500

    Wrote Interface

--- src/HaadCRM.Service/Services/GroupStudents/IGroupStudent.cs
+++ src/HaadCRM.Service/Services/GroupStudents/IGroupStudent.cs
@@ -1,5 +1,12 @@
-﻿namespace HaadCRM.Service.Services.GroupStudents;
+﻿using HaadCRM.Service.DTOs.GroupDTOs.GroupStudents;
+
+namespace HaadCRM.Service.Services.GroupStudents;
 
 public interface IGroupStudent
 {
+    ValueTask<GroupStudentViewModel> CreateAsync(GroupStudentCreateModel groupStudent);
+    ValueTask<GroupStudentViewModel> UpdateAsync(long id, GroupStudentUpdateModel groupStudent);
+    ValueTask<bool> DeleteAsync(long id);
+    ValueTask<GroupStudentViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<GroupStudentViewModel>> GetAllAsync();
 }
