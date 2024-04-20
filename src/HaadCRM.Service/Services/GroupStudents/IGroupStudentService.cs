2699933fae7b39702fb1c4976a2219cab6078c23
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-21 00:46:25 +0500

    Wrote Comments to IGroupStudentsService

--- src/HaadCRM.Service/Services/GroupStudents/IGroupStudentService.cs
+++ src/HaadCRM.Service/Services/GroupStudents/IGroupStudentService.cs
@@ -4,9 +4,34 @@ namespace HaadCRM.Service.Services.GroupStudents;
 
 public interface IGroupStudentService
 {
+    /// <summary>
+    /// Creates new groupStudent
+    /// </summary>
+    /// <param name="groupStudent"></param>
+    /// <returns></returns>
     ValueTask<GroupStudentViewModel> CreateAsync(GroupStudentCreateModel groupStudent);
+    /// <summary>
+    /// Updates already exist groupStudent
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="groupStudent"></param>
+    /// <returns></returns>
     ValueTask<GroupStudentViewModel> UpdateAsync(long id, GroupStudentUpdateModel groupStudent);
+    /// <summary>
+    /// Deletes already exist groupStudent
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     ValueTask<bool> DeleteAsync(long id);
+    /// <summary>
+    /// Gets groupStudent by Id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     ValueTask<GroupStudentViewModel> GetByIdAsync(long id);
+    /// <summary>
+    /// Will get all groupStudents and returns IEnumerable
+    /// </summary>
+    /// <returns></returns>
     ValueTask<IEnumerable<GroupStudentViewModel>> GetAllAsync();
 }
