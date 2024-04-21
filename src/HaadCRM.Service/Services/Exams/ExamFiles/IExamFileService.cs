1d0d5e27380df24283ab016615fb8c8b4777f4a5
Jayxun <140449492+fullstack-Maestro@users.noreply.github.com>
2024-04-21 05:15:37 -0700

    Create ExamFile Service

---
+++ src/HaadCRM.Service/Services/Exams/ExamFiles/IExamFileService.cs
@@ -0,0 +1,12 @@
+﻿using HaadCRM.Service.DTOs.ExamDTOs.ExamFiles;
+
+namespace HaadCRM.Service.Services.Exams.ExamFiles;
+
+public interface IExamFileService
+{
+    ValueTask<ExamFileViewModel> CreateAsync(ExamFileCreateModel createModel);
+    ValueTask<ExamFileViewModel> GetByIdAsync(long id);
+    ValueTask<IEnumerable<ExamFileViewModel>> GetAllAsync();
+    ValueTask<ExamFileViewModel> UpdateAsync(long id, ExamFileUpdateModel updateModel);
+    ValueTask<bool> DeleteAsync(long id);
+}
