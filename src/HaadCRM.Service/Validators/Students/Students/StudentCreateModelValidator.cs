985985f36a5102d2b157498ce43e88009edc4e12
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-24 00:50:39 +0500

    RemovedStudentCreateModelValidator, StudentCreateModelValidator

---
+++ src/HaadCRM.Service/Validators/Students/Students/StudentCreateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.StudentDTOs.Students;
+
+namespace HaadCRM.Service.Validators.Students.Students;
+
+public class StudentCreateModelValidator : AbstractValidator<StudentCreateModel>
+{
+    public StudentCreateModelValidator()
+    {
+        RuleFor(model => model.UserId)
+            .NotEmpty().WithMessage("User ID must be specified.")
+            .GreaterThan(0).WithMessage("User ID must be greater than 0.");
+
+        RuleFor(model => model.AssetId)
+            .NotEmpty().WithMessage("Asset ID must be specified.")
+            .GreaterThan(0).WithMessage("Asset ID must be greater than 0.");
+    }
+}
---
+++ src/HaadCRM.Service/Validators/Students/Students/StudentUpdateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.StudentDTOs.Students;
+
+namespace HaadCRM.Service.Validators.Students.Students;
+
+public class StudentUpdateModelValidator : AbstractValidator<StudentUpdateModel>
+{
+    public StudentUpdateModelValidator()
+    {
+        RuleFor(model => model.UserId)
+            .NotEmpty().WithMessage("User ID must be specified.")
+            .GreaterThan(0).WithMessage("User ID must be greater than 0.");
+
+        RuleFor(model => model.AssetId)
+            .NotEmpty().WithMessage("Asset ID must be specified.")
+            .GreaterThan(0).WithMessage("Asset ID must be greater than 0.");
+    }
+}
