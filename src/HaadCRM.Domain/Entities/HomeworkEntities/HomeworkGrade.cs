2f5c4a6afd2d8ef7eff5bd009e4cae7b7287de95
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 03:24:40 +0500

    Wrote comments for HomeworkGrade entity

--- src/HaadCRM.Domain/Entities/HomeworkEntities/HomeworkGrade.cs
+++ src/HaadCRM.Domain/Entities/HomeworkEntities/HomeworkGrade.cs
@@ -17,12 +17,33 @@ namespace HaadCRM.Domain.Entities.Homeworks;
 /// </summary>
 public class HomeworkGrade : Auditable
 {
+    /// <summary>
+    /// The StudentId property represents the Id of the Student
+    /// </summary>
     public long StudentId { get; set; }
+    /// <summary>
+    /// The AssistantId property represents the If of the Assistant
+    /// </summary>
     public long AssistantId { get; set; }
+    /// <summary>
+    /// The HomeworkId property represents the ID of the Homework
+    /// </summary>
     public long HomeworkId { get; set; }
+    /// <summary>
+    /// The Grade property represents the Student's grade to his homework
+    /// </summary>
     public int Grade { get; set; }
 
+    /// <summary>
+    /// The Homework property represents the Homework object
+    /// </summary>
     public Homework Homework { get; set; }
+    /// <summary>
+    /// The Assistant property represents the Assistant object
+    /// </summary>
     public Employee Assistant { get; set; }
+    /// <summary>
+    /// The Student property represents the Student object
+    /// </summary>
     public Student Student { get; set; }
 }
