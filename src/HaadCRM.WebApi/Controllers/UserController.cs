ced7cd251b9414f35679f7c8e083af92c8d03000
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-23 18:23:09 +0500

    Finished UserController

--- src/HaadCRM.WebApi/Controllers/UserController.cs
+++ src/HaadCRM.WebApi/Controllers/UserController.cs
@@ -1,71 +1,131 @@
-﻿using HaadCRM.Service.Services.Users;
+﻿using HaadCRM.Service.DTOs.UserDTOs.Users;
+using HaadCRM.Service.Services.Users;
 using HaadCRM.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HaadCRM.WebApi.Controllers
+namespace HaadCRM.WebApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController(IUserService userService) : BaseController
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class UsersController(IUserService userService) : ControllerBase
+    [HttpPost("api/[controller]")]
+    public async ValueTask<IActionResult> CreateAsync([FromBody] UserCreateModel createModel)
     {
-        [HttpGet("login")]
-        public async ValueTask<IActionResult> LoginAsync(string phone, string password)
+        var createdUser = await userService.CreateAsync(createModel);
+        return Ok(new Response
         {
-            var result = await userService.LoginAsync(phone, password);
-            return Ok(new Response
-            {
-                StatusCode = 200,
-                Message = "Ok",
-                Data = result
-            });
-        }
+            StatusCode = 200,
+            Message = "Success",
+            Data = createdUser
+        });
+    }
 
-        [HttpGet("send-code")]
-        public async ValueTask<IActionResult> SendCodeAsync(string phone)
+    [HttpPut("api/[controller]/{id}")]
+    public async ValueTask<IActionResult> UpdateAsync(long id, [FromBody] UserUpdateModel updateModel)
+    {
+        var updatedUser = await userService.UpdateAsync(id, updateModel);
+        return Ok(new Response
         {
-            var result = await userService.SendCodeAsync(phone);
-            return Ok(new Response
-            {
-                StatusCode = 200,
-                Message = "Ok",
-                Data = result
-            });
-        }
+            StatusCode = 200,
+            Message = "Success",
+            Data = updatedUser
+        });
+    }
 
-        [HttpGet("confirm-code")]
-        public async ValueTask<IActionResult> ConfirmAsync(string phone, string code)
+    [HttpDelete("api/[controller]/{id}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        var result = await userService.DeleteAsync(id);
+        return Ok(new Response
         {
-            var result = await userService.ConfirmCodeAsync(phone, code);
-            return Ok(new Response
-            {
-                StatusCode = 200,
-                Message = "Ok",
-                Data = result
-            });
-        }
+            StatusCode = 200,
+            Message = "Success",
+            Data = result
+        });
+    }
 
-        [HttpPatch("reset-password")]
-        public async ValueTask<IActionResult> ResetPasswordAsync(string phone, string newPassword)
+    [HttpGet("api/[controller]")]
+    public async ValueTask<IActionResult> GetAllAsync()
+    {
+        var users = await userService.GetAllAsync();
+        return Ok(new Response
         {
-            var result = await userService.ResetPasswordAsync(phone, newPassword);
-            return Ok(new Response
-            {
-                StatusCode = 200,
-                Message = "Ok",
-                Data = result
-            });
-        }
+            StatusCode = 200,
+            Message = "Success",
+            Data = users
+        });
+    }
 
-        [HttpPatch("change-password")]
-        public async ValueTask<IActionResult> ChangePasswordAsync(string oldPassword, string newPassword)
+    [HttpGet("api/[controller]/{id}")]
+    public async ValueTask<IActionResult> GetByIdAsync(long id)
+    {
+        var user = await userService.GetByIdAsync(id);
+        return Ok(new Response
         {
-            var result = await userService.ChangePasswordAsync(oldPassword, newPassword);
-            return Ok(new Response
-            {
-                StatusCode = 200,
-                Message = "Ok",
-                Data = result
-            });
-        }
+            StatusCode = 200,
+            Message = "Success",
+            Data = user
+        });
+    }
+
+    [HttpGet("login")]
+    public async ValueTask<IActionResult> LoginAsync(string phone, string password)
+    {
+        var result = await userService.LoginAsync(phone, password);
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = result
+        });
+    }
+
+    [HttpGet("send-code")]
+    public async ValueTask<IActionResult> SendCodeAsync(string phone)
+    {
+        var result = await userService.SendCodeAsync(phone);
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = result
+        });
+    }
+
+    [HttpGet("confirm-code")]
+    public async ValueTask<IActionResult> ConfirmAsync(string phone, string code)
+    {
+        var result = await userService.ConfirmCodeAsync(phone, code);
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = result
+        });
+    }
+
+    [HttpPatch("reset-password")]
+    public async ValueTask<IActionResult> ResetPasswordAsync(string phone, string newPassword)
+    {
+        var result = await userService.ResetPasswordAsync(phone, newPassword);
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = result
+        });
+    }
+
+    [HttpPatch("change-password")]
+    public async ValueTask<IActionResult> ChangePasswordAsync(string oldPassword, string newPassword)
+    {
+        var result = await userService.ChangePasswordAsync(UserPhone, oldPassword, newPassword);
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = result
+        });
     }
 }
