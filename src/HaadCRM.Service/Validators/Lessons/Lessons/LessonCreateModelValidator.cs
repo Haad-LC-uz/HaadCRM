5452c358099f1bcb3742a6e7f95ebf9ac9055182
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-24 00:48:22 +0500

    LessonCreateModelValidator

---
+++ src/HaadCRM.Service/Validators/Lessons/Lessons/LessonCreateModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.LessonsDTOs.Lessons;
+
+namespace HaadCRM.Service.Validators.Lessons.Lessons;
+
+public class LessonCreateModelValidator : AbstractValidator<LessonCreateModel>
+{
+    public LessonCreateModelValidator()
+    {
+        RuleFor(model => model.GroupId)
+            .NotEmpty().WithMessage("Group ID must be specified.")
+            .GreaterThan(0).WithMessage("Group ID must be greater than 0.");
+
+        RuleFor(model => model.DateOfLesson)
+            .NotEmpty().WithMessage("Date of lesson must be specified.")
+            .Must(date => date > DateTime.MinValue).WithMessage("Date of lesson must be a valid date.");
+
+        RuleFor(model => model.Name)
+            .NotEmpty().WithMessage("Lesson name must be specified.")
+            .MaximumLength(255).WithMessage("Lesson name cannot exceed 255 characters.");
+    }
+}
