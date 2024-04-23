350e59a7845b69458c27362c7b341dcfaf240ca6
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 03:18:22 +0500

    Wrote comments for HomeworkFile entity

--- src/HaadCRM.Domain/Entities/HomeworkEntities/HomeworkFile.cs
+++ src/HaadCRM.Domain/Entities/HomeworkEntities/HomeworkFile.cs
@@ -10,9 +10,21 @@ namespace HaadCRM.Domain.Entities.Homeworks;
 /// </summary>
 public class HomeworkFile : Auditable
 {
+    /// <summary>
+    /// The HomeworkId property represents the Id of the Homework
+    /// </summary>
     public long HomeworkId { get; set; }
+    /// <summary>
+    /// The AssetId property represents the Id of the Asset
+    /// </summary>
     public long AssetId { get; set; }
 
+    /// <summary>
+    /// The Homework property represents the Homework object
+    /// </summary>
     public Homework Homework { get; set; }
+    /// <summary>
+    /// The Asset property represents the Asset object
+    /// </summary>
     public Asset Asset { get; set; }
 }
