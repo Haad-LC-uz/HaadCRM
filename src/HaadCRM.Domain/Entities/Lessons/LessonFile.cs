949e9c6f746f037f8dd1da40dd3799928be05fa8
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 03:56:46 +0500

    Wrote comments for class

--- src/HaadCRM.Domain/Entities/Lessons/LessonFile.cs
+++ src/HaadCRM.Domain/Entities/Lessons/LessonFile.cs
@@ -2,6 +2,14 @@
 
 namespace HaadCRM.Domain.Entities.Lessons;
 
+/// <summary>
+/// The LessonFile class represents the LessonFile entity that contains proprerties for LessonFile,
+/// such as:
+/// LessonId
+/// AssetId
+/// Lesson
+/// Asset
+/// </summary>
 public class LessonFile : Auditable
 {
     public long LessonId { get; set; }
