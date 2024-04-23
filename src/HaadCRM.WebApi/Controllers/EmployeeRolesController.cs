0aaa2186dd478ecc4cd2ffeda5ea09d113af36c8
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-23 17:02:36 +0500

    Added EmployeeRole Controller

---
+++ src/HaadCRM.WebApi/Controllers/EmployeeRolesController.cs
@@ -0,0 +1,64 @@
+﻿using HaadCRM.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using HaadCRM.Service.Services.EmployeeRoles;
+using HaadCRM.Service.DTOs.EmployeeDTOs.EmployeeRoles;
+
+namespace HaadCRM.WebApi.Controllers;
+
+public class EmployeeRolesController(IEmployeeRoleService employeeRoleService) : BaseController
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] EmployeeRoleCreateModel createModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await employeeRoleService.CreateAsync(createModel)
+        });
+    }
+
+    [HttpPut("{id:long}")]
+    public async ValueTask<IActionResult> PutAsync(long id, [FromBody] EmployeeRoleUpdateModel updateModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await employeeRoleService.UpdateAsync(id, updateModel)
+        });
+    }
+
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await employeeRoleService.DeleteAsync(id)
+        });
+    }
+
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await employeeRoleService.GetByIdAsync(id)
+        });
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAsync()
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await employeeRoleService.GetAllAsync()
+        });
+    }
+}
