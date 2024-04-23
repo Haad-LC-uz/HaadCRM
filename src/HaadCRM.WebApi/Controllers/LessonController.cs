e2e013cbbc8269256f01e4c8894dff4ac30343be
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-23 23:06:49 +0500

    Created LessonController and wrote PostASync method

---
+++ src/HaadCRM.WebApi/Controllers/LessonController.cs
@@ -0,0 +1,23 @@
+﻿using HaadCRM.Service.DTOs.LessonsDTOs.Lessons;
+using HaadCRM.Service.Services.Lessons;
+using HaadCRM.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaadCRM.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LessonController(ILessonService lessonService) : ControllerBase
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] LessonCreateModel lesson)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await lessonService.CreateAsync(lesson)
+        });
+    }
+}
+
