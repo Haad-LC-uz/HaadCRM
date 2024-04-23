c88387debed5bc92d62745c7086b64f45336a44b
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-24 00:41:54 +0500

    HomeworkFileCreateModelValidator

---
+++ src/HaadCRM.Service/Validators/Homework/HomeworkFiles/HomeworkFileCreateModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.HomeworkDTOs.HomeworkFiles;
+
+namespace HaadCRM.Service.Validators.Homework.HomeworkFiles;
+
+public class HomeworkFileCreateModelValidator : AbstractValidator<HomeworkFileCreateModel>
+{
+    public HomeworkFileCreateModelValidator()
+    {
+        RuleFor(x => x.HomeworkId)
+            .NotEmpty().WithMessage("Homework ID must be specified.")
+            .GreaterThan(0).WithMessage("Homework ID must be greater than 0.");
+        RuleFor(x => x.AssetId)
+            .NotEmpty().WithMessage("Asset ID must be specified.")
+            .GreaterThan(0).WithMessage("Homework ID must be greater than 0.");
+    }
+}
