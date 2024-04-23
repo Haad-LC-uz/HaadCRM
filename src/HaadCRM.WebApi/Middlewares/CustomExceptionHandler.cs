b6e48e53e610b2d4bf48a686bbb9fb1e258c2b65
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-23 11:07:47 +0500

    Add ExceptionHandlerMiddleware

---
+++ src/HaadCRM.WebApi/Middlewares/CustomExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using HaadCRM.WebApi.Models;
+using HaadCRM.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace HaadCRM.WebApi.Middlewares;
+
+public class CustomExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        if (exception is not CustomException customException)
+            return false;
+
+        await httpContext.Response.WriteAsJsonAsync(new Response
+        {
+            StatusCode = customException.StatusCode,
+            Message = customException.Message,
+        });
+
+        return true;
+    }
+}
