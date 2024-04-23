9687e5b4cfbb554b845b766798f237b84d89826a
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 04:12:35 +0500

    Wrote comments for LessonFile entity

--- src/HaadCRM.Domain/Entities/Lessons/LessonFile.cs
+++ src/HaadCRM.Domain/Entities/Lessons/LessonFile.cs
@@ -12,9 +12,21 @@ namespace HaadCRM.Domain.Entities.Lessons;
 /// </summary>
 public class LessonFile : Auditable
 {
+    /// <summary>
+    /// The LessonId property represents the Id of the Lesson
+    /// </summary>
     public long LessonId { get; set; }
+    /// <summary>
+    /// The AssetId property represents the Id of the Asset
+    /// </summary>
     public long AssetId { get; set; }
 
+    /// <summary>
+    /// The Lesson property represents the Lesson object
+    /// </summary>
     public Lesson Lesson { get; set; }
+    /// <summary>
+    /// The Asset property represents the Asset object
+    /// </summary>
     public Asset Asset { get; set; }
 }
