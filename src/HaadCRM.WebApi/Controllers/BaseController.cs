3406fa3f8c74b3a963aa2e0fddfebff84201e0dd
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-23 18:18:28 +0500

    Changed BaseController and added some menthods in UserController

--- src/HaadCRM.WebApi/Controllers/BaseController.cs
+++ src/HaadCRM.WebApi/Controllers/BaseController.cs
@@ -6,4 +6,8 @@ namespace HaadCRM.WebApi.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 [Authorize]
-public abstract class BaseController : ControllerBase { }
+public class BaseController : ControllerBase
+{
+    public string UserPhone => HttpContext?.User?.FindFirst("Phone")?.Value;
+    public long GetUserId => Convert.ToInt64(HttpContext?.User?.FindFirst("Id")?.Value);
+}
---
+++ src/HaadCRM.WebApi/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using HaadCRM.Service.Services.Users;
+using HaadCRM.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaadCRM.WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController(IUserService userService) : ControllerBase
+    {
+        [HttpGet("login")]
+        public async ValueTask<IActionResult> LoginAsync(string phone, string password)
+        {
+            var result = await userService.LoginAsync(phone, password);
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Ok",
+                Data = result
+            });
+        }
+
+        [HttpGet("send-code")]
+        public async ValueTask<IActionResult> SendCodeAsync(string phone)
+        {
+            var result = await userService.SendCodeAsync(phone);
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Ok",
+                Data = result
+            });
+        }
+
+        [HttpGet("confirm-code")]
+        public async ValueTask<IActionResult> ConfirmAsync(string phone, string code)
+        {
+            var result = await userService.ConfirmCodeAsync(phone, code);
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Ok",
+                Data = result
+            });
+        }
+
+        [HttpPatch("reset-password")]
+        public async ValueTask<IActionResult> ResetPasswordAsync(string phone, string newPassword)
+        {
+            var result = await userService.ResetPasswordAsync(phone, newPassword);
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Ok",
+                Data = result
+            });
+        }
+
+        [HttpPatch("change-password")]
+        public async ValueTask<IActionResult> ChangePasswordAsync(string oldPassword, string newPassword)
+        {
+            var result = await userService.ChangePasswordAsync(oldPassword, newPassword);
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Ok",
+                Data = result
+            });
+        }
+    }
+}
