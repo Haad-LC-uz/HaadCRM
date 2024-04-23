5b2478585ce11011edbfc619a37be0080003571b
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 00:07:54 +0500

    Wrote PostAsync method

--- src/HaadCRM.WebApi/Controllers/HomeworkController.cs
+++ src/HaadCRM.WebApi/Controllers/HomeworkController.cs
@@ -1,4 +1,6 @@
-﻿using HaadCRM.Service.Services.HomeworkServices;
+﻿using HaadCRM.Service.DTOs.HomeworkDTOs.Homework;
+using HaadCRM.Service.Services.HomeworkServices;
+using HaadCRM.WebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HaadCRM.WebApi.Controllers;
@@ -7,4 +9,14 @@ namespace HaadCRM.WebApi.Controllers;
 [ApiController]
 public class HomeworkController(IHomeWorkService homeWorkService) : ControllerBase
 {
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] HomeworkCreateModel homework)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await homeWorkService.CreateAsync(homework)
+        });
+    }
 }
