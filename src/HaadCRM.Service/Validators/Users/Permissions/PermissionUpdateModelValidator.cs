ebac1e8013deed111e963552337e85ea0b50096f
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-24 00:56:31 +0500

    PermissionCreateModelValidator, UserPermissionCreateModelValidator, UserRoleCreateModelValidator

---
+++ src/HaadCRM.Service/Validators/Users/Permissions/PermissionUpdateModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.UserDTOs.Permissions;
+
+namespace HaadCRM.Service.Validators.Users.Permissions;
+
+public class PermissionUpdateModelValidator : AbstractValidator<PermissionUpdateModel>
+{
+    public PermissionUpdateModelValidator()
+    {
+        RuleFor(model => model.Method)
+            .NotEmpty().WithMessage("Method must be specified.");
+
+        RuleFor(model => model.Controller)
+            .NotEmpty().WithMessage("Controller must be specified.");
+    }
+}
