693191d7edf697469a1e7f0cb76af3582e4ce92f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 01:37:59 +0500

    Wrote comments for Course entity

--- src/HaadCRM.Domain/Entities/Courses/Course.cs
+++ src/HaadCRM.Domain/Entities/Courses/Course.cs
@@ -3,10 +3,23 @@ using HaadCRM.Domain.Entities.Groups;
 
 namespace HaadCRM.Domain.Entities.Courses;
 
+/// <summary>
+/// The Couse class represents a Course that contains properties for course,
+/// such as Course's name and Description
+/// </summary>
 public class Course : Auditable
 {
+    /// <summary>
+    /// The Name property represents the name of the Course
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// The Description property represents the description of the Course
+    /// </summary>
     public string Description { get; set; }
 
+    /// <summary>
+    /// The Groups property represents the list of Groups that course Contains
+    /// </summary>
     public IEnumerable<Group> Groups { get; set; }
 }
