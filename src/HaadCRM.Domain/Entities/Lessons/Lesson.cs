79b36e194228ea090ae4e3e5e40ce770829316ec
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 03:34:54 +0500

    Wrote comments for class

--- src/HaadCRM.Domain/Entities/Lessons/Lesson.cs
+++ src/HaadCRM.Domain/Entities/Lessons/Lesson.cs
@@ -3,8 +3,18 @@ using HaadCRM.Domain.Entities.Groups;
 
 namespace HaadCRM.Domain.Entities.Lessons;
 
+/// <summary>
+/// The Lesson class represents the Lesson entity that contains properties for Lesson, 
+/// such as:
+/// GroupId
+/// DateOfLesson
+/// Name
+/// Group
+/// Files
+/// </summary>
 public class Lesson : Auditable
 {
+
     public long GroupId { get; set; }
     public DateTime DateOfLesson { get; set; }
     public string Name { get; set; }
