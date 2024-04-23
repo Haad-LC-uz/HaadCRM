7e7a34dbb2ed5f55be2ca0e5cdfcbad360342722
Komiljon <komiljonrahmatov03@gmail.com>
2024-04-23 23:55:29 +0500

    Added Attendance Controller

---
+++ src/HaadCRM.WebApi/Controllers/AttendancesController.cs
@@ -0,0 +1,64 @@
+﻿using HaadCRM.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using HaadCRM.Service.DTOs.Attendances;
+using HaadCRM.Service.Services.Attendances;
+
+namespace HaadCRM.WebApi.Controllers;
+
+public class AttendancesController(IAttendanceService attendanceService) : BaseController
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostAsync([FromBody] AttendanceCreateModel createModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await attendanceService.CreateAsync(createModel)
+        });
+    }
+
+    [HttpPut("{id:long}")]
+    public async ValueTask<IActionResult> PutAsync(long id, [FromBody] AttendanceUpdateModel updateModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await attendanceService.UpdateAsync(id, updateModel)
+        });
+    }
+
+    [HttpDelete("{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await attendanceService.DeleteAsync(id)
+        });
+    }
+
+    [HttpGet("{id:long}")]
+    public async ValueTask<IActionResult> GetAsync(long id)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await attendanceService.GetByIdAsync(id)
+        });
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAsync()
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await attendanceService.GetAllAsync()
+        });
+    }
+}
