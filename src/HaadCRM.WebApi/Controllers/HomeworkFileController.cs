84e02843e5a6f0a85463b3a104ab77c80b01b069
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 00:24:42 +0500

    edited

--- src/HaadCRM.WebApi/Controllers/HomeworkFileController.cs
+++ src/HaadCRM.WebApi/Controllers/HomeworkFileController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HaadCRM.Service.Services.HomeworkFiles;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HaadCRM.WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class HomeworkFileController : ControllerBase
+public class HomeworkFileController(IHomeworkFilesService homeworkFilesService) : ControllerBase
 {
 }
