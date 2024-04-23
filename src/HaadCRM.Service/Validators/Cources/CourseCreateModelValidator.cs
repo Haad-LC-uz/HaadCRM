46a8ea1bf02d1a7bb0f79cec442fc7f3a25fd8eb
Asrorbek-Abrorov <as.abrorov@gmail.com>
2024-04-24 00:19:18 +0500

    Cources

---
+++ src/HaadCRM.Service/Validators/Cources/CourseCreateModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HaadCRM.Service.DTOs.Courses;
+
+namespace HaadCRM.Service.Validators.Cources;
+
+public class CourseCreateModelValidator : AbstractValidator<CourseCreateModel>
+{
+    public CourseCreateModelValidator()
+    {
+        RuleFor(model => model.Name)
+            .NotEmpty().WithMessage("Name must be specified.");
+
+        RuleFor(model => model.Description)
+            .NotEmpty().WithMessage("Description must be specified.");
+    }
+}
