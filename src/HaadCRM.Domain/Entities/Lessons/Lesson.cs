e8f3811a8b3f37c069bca77e1484fed36590f92b
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 03:37:39 +0500

    Wrote Comments for Lesson entity

--- src/HaadCRM.Domain/Entities/Lessons/Lesson.cs
+++ src/HaadCRM.Domain/Entities/Lessons/Lesson.cs
@@ -14,12 +14,26 @@ namespace HaadCRM.Domain.Entities.Lessons;
 /// </summary>
 public class Lesson : Auditable
 {
-
+    /// <summary>
+    /// The GroupId property represents the Id of the Group
+    /// </summary>
     public long GroupId { get; set; }
+    /// <summary>
+    /// The DateOfLesson property represents the Date of the Lesson
+    /// </summary>
     public DateTime DateOfLesson { get; set; }
+    /// <summary>
+    /// The Name property represents the name of the Lesson
+    /// </summary>
     public string Name { get; set; }
-
+    
+    /// <summary>
+    /// The Group property represents the Group object
+    /// </summary>
     public Group Group { get; set; }
 
+    /// <summary>
+    /// The Files property represents the list of files that Lesson has
+    /// </summary>
     public IEnumerable<LessonFile> Files { get; set; }
 }
