8125c5436938b427e39e816b2f654fe204c41c11
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 01:33:48 +0500

    Wrote comments for Attendance entity

--- src/HaadCRM.Domain/Entities/Attendances/Attendance.cs
+++ src/HaadCRM.Domain/Entities/Attendances/Attendance.cs
@@ -4,12 +4,31 @@ using HaadCRM.Domain.Entities.Students;
 
 namespace HaadCRM.Domain.Entities.Attendances;
 
+/// <summary>
+/// The Attendance class represents Attendance object that contains properties for student's attendance,
+/// such as Data and Attendance status
+/// </summary>
 public class Attendance : Auditable
 {
+    /// <summary>
+    /// The StudentId property represents the Id of Student
+    /// </summary>
     public long StudentId { get; set; }
+    /// <summary>
+    /// The LessonId Property represent the Id of Lesson
+    /// </summary>
     public long LessonId { get; set; }
+    /// <summary>
+    /// The IsAttended property represents the status of Attendance
+    /// </summary>
     public bool IsAttended { get; set; }
 
+    /// <summary>
+    /// The Student property represents the Student object
+    /// </summary>
     public Student Student { get; set; }
+    /// <summary>
+    /// The Lesson property represents the Lesson object
+    /// </summary>
     public Lesson Lesson { get; set; }
 }
