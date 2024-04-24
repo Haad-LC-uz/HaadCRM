cab3b1bba295ac18848cc6f58d2a5280b995b470
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:22:17 +0500

    Wrote comments for class

--- src/HaadCRM.Domain/Entities/Users/UserRole.cs
+++ src/HaadCRM.Domain/Entities/Users/UserRole.cs
@@ -2,6 +2,11 @@
 
 namespace HaadCRM.Domain.Entities.Users;
 
+/// <summary>
+/// The UserRole class represents the UserRole entity that contains properties for User role,
+/// such as:
+/// Name
+/// </summary>
 public class UserRole : Auditable
 {
     public string Name { get; set; }
