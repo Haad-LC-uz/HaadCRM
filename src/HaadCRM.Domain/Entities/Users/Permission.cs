30eda1bba62fdcb57de199299a1996138bdf03f3
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:48:46 +0500

    wrote comments for Permission entity

--- src/HaadCRM.Domain/Entities/Users/Permission.cs
+++ src/HaadCRM.Domain/Entities/Users/Permission.cs
@@ -2,8 +2,15 @@ using HaadCRM.Domain.Commons;
 
 namespace HaadCRM.Domain.Entities.Users;
 
+
 public class Permission : Auditable
 {
+    /// <summary>
+    /// The Method property represents what method is target of the permission
+    /// </summary>
     public string Method { get; set; }
+    /// <summary>
+    /// The Contoller property represents what Type of user is target of the permission
+    /// </summary>
     public string Controller { get; set; }
 }
