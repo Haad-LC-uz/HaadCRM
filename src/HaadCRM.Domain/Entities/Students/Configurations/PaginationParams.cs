57d451a3c0b8797602075a527711ee248133693e
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:50:20 +0500

    Wrote Comments for Class

--- src/HaadCRM.Domain/Entities/Students/Configurations/PaginationParams.cs
+++ src/HaadCRM.Domain/Entities/Students/Configurations/PaginationParams.cs
@@ -1,5 +1,11 @@
 ﻿namespace HaadCRM.Service.Configurations;
 
+/// <summary>
+/// The PaginationParams class represents the PaginationParams entity that contains properties for PaginationParams,
+/// such as:
+/// PageIndex
+/// PageSize
+/// </summary>
 public class PaginationParams
 {
     public int PageIndex { get; set; } = 1;
