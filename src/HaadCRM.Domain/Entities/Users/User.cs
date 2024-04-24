690bf8a67fa62ef703cacd53f814fdfe6cf8f8ad
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:27:32 +0500

    Wrote Comments for class

--- src/HaadCRM.Domain/Entities/Users/User.cs
+++ src/HaadCRM.Domain/Entities/Users/User.cs
@@ -2,6 +2,17 @@
 
 namespace HaadCRM.Domain.Entities.Users;
 
+/// <summary>
+/// The User class represents the User entity that contains properties for User,
+/// such as
+/// FirstName
+/// LastName
+/// Email
+/// Password
+/// Phone
+/// UserRoleId
+/// UserRole
+/// </summary>
 public class User : Auditable
 {
     public string FirstName { get; set; }
