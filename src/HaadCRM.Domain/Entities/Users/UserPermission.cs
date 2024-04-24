bf7a228e916955d013f8488962b3671720b253d1
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:24:09 +0500

    Wrote comments for class

--- src/HaadCRM.Domain/Entities/Users/UserPermission.cs
+++ src/HaadCRM.Domain/Entities/Users/UserPermission.cs
@@ -2,6 +2,14 @@ using HaadCRM.Domain.Commons;
 
 namespace HaadCRM.Domain.Entities.Users;
 
+/// <summary>
+/// The UserPermission class represents the UserPermission entity that contains properties for UserPermission,
+/// such as"
+/// UserId
+/// PermissionId
+/// User
+/// Permission
+/// </summary>
 public class UserPermission : Auditable
 {
     public long UserId { get; set; }
