d219246473a3b28d6ccdb6abdbca5fe0ee95084b
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-04-24 05:02:27 +0500

    Wrote comments for RemovedStudent entity

--- src/HaadCRM.Domain/Entities/Students/RemovedStudent.cs
+++ src/HaadCRM.Domain/Entities/Students/RemovedStudent.cs
@@ -5,10 +5,25 @@ namespace HaadCRM.Domain.Entities.Students;
 
 public class RemovedStudent : Auditable
 {
+    /// <summary>
+    /// The StudentId property represents the Id of the Student
+    /// </summary>
     public long StudentId { get; set; }
+    /// <summary>
+    /// The GroupId property represents the Id of the Group
+    /// </summary>
     public long GroupId { get; set; }
+    /// <summary>
+    /// The Reason property represents the reason that why Students was removed
+    /// </summary>
     public string Reason { get; set; }
 
+    /// <summary>
+    /// The Group property represents the Group object
+    /// </summary>
     public Group Group { get; set; }
+    /// <summary>
+    /// The Student property represents the Student object
+    /// </summary>
     public Student Student { get; set; }
 }
